b7cbbf7621f51d16be670d44a9bfab3f786e5959
Carlos Manuel <fis.carlosmanuel@gmail.com>
2025-06-12 22:54:28 -0600

    Restore SelectRect event handling

--- wxChaos/gui/SelectRect.cpp
+++ wxChaos/gui/SelectRect.cpp
@@ -1,4 +1,5 @@
 #include "SelectRect.h"
+#include <algorithm>
 
 SelectRect::SelectRect(sf::RenderWindow* Window)
     : x(-1), y(-1), xSelect(-1), ySelect(-1), inSelection(false)
@@ -22,12 +23,89 @@ void SelectRect::Show(sf::RenderWindow* Window)
 
 bool SelectRect::HandleEvents(sf::Event Event)
 {
+    if(Event.type == sf::Event::MouseButtonPressed)
+    {
+        if(Event.mouseButton.button == sf::Mouse::Left)
+        {
+            pos.top = Event.mouseButton.y;
+            pos.left = Event.mouseButton.x;
+
+            pos.width = 0;
+            pos.height = 0;
+
+            xSelect = Event.mouseButton.x;
+            ySelect = Event.mouseButton.y;
+
+            inSelection = true;
+        }
+    }
+
+    if(Event.type == sf::Event::MouseMoved && inSelection)
+    {
+        x = Event.mouseMove.x;
+        y = Event.mouseMove.y;
+
+        pos.left = std::min(x, xSelect);
+        pos.top  = std::min(y, ySelect);
+        pos.width  = std::abs(x - xSelect);
+        pos.height = std::abs(y - ySelect);
+    }
+
+    if(Event.type == sf::Event::MouseButtonReleased)
+    {
+        if(Event.mouseButton.button == sf::Mouse::Left)
+        {
+            select = pos;
+            inSelection = false;
+            if(pos.height != 0 && pos.width != 0)
+                return true;
+        }
+    }
     return false;
 }
 
-void SelectRect::ClickEvent(wxMouseEvent& event){}
-bool SelectRect::UnClickEvent(wxMouseEvent& event){return false;}
-void SelectRect::MoveEvent(wxMouseEvent& event){}
+void SelectRect::ClickEvent(wxMouseEvent& event)
+{
+    if(event.ButtonDown(wxMOUSE_BTN_LEFT))
+    {
+        pos.top = event.GetPosition().y;
+        pos.left = event.GetPosition().x;
+
+        pos.width = 0;
+        pos.height = 0;
+
+        xSelect = event.GetPosition().x;
+        ySelect = event.GetPosition().y;
+
+        inSelection = true;
+    }
+}
+
+bool SelectRect::UnClickEvent(wxMouseEvent& event)
+{
+    if(event.ButtonUp(wxMOUSE_BTN_LEFT))
+    {
+        select = pos;
+        inSelection = false;
+        if(pos.height != 0 && pos.width != 0)
+            return true;
+    }
+    return false;
+}
+
+void SelectRect::MoveEvent(wxMouseEvent& event)
+{
+    if(inSelection)
+    {
+        x = event.GetPosition().x;
+        y = event.GetPosition().y;
+
+        pos.left = std::min(x, xSelect);
+        pos.top  = std::min(y, ySelect);
+        pos.width  = std::abs(x - xSelect);
+        pos.height = std::abs(y - ySelect);
+    }
+}
 
 sf::IntRect SelectRect::GetSeleccion()
 {
