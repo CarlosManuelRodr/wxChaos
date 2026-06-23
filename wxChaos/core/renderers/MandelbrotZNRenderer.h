220c01e8e81db472897b6ec33357d78bce90ee03
Carlos Manuel <fis.carlosmanuel@gmail.com>
2026-06-23 01:04:00 -0600

    Roll back perturbation rendering on MandelbrotZN

--- wxChaos/core/renderers/MandelbrotZNRenderer.h
+++ wxChaos/core/renderers/MandelbrotZNRenderer.h
@@ -1,21 +1,17 @@
 #pragma once
-#include "PerturbationRenderer.h"
+#include "../Renderer.h"
 
 /*
 * @class RenderMandelbrotZN
 * @brief Threaded MandelbrotZN rendering routines.
 */
-class MandelbrotZNRenderer : public PerturbationRenderer
+class MandelbrotZNRenderer : public Renderer
 {
     int _n;
     double _bailout;
 
     template<class Real, class MeasurePoint>
     Point TracePoint(const Real& pixelRe, const Real& pixelIm, MeasurePoint measure) const;
-    [[nodiscard]] PerturbationReference BuildPerturbationReference(const HighPrecisionReal& centerRe, const HighPrecisionReal& centerIm) const;
-    template<class MeasurePoint>
-    PerturbationTraceResult TracePerturbationPoint(const HighPrecisionReal& pixelRe, const HighPrecisionReal& pixelIm,
-                                                   const PerturbationReference& reference, MeasurePoint measure) const;
 
 public:
     MandelbrotZNRenderer();
