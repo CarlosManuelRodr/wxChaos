9470decf48a4e5ce7fbdbc39357780075678f2f5
Carlos Manuel <fis.carlosmanuel@gmail.com>
2026-07-12 01:46:37 -0600

    Refine `ScriptNameDialog` layout, add section header, and update Spanish localization.

--- wxChaos/gui/wx/scripting/ScriptNameDialog.cpp
+++ wxChaos/gui/wx/scripting/ScriptNameDialog.cpp
@@ -1,4 +1,6 @@
 #include "ScriptNameDialog.h"
+
+#include <algorithm>
 #include <wx/xrc/xmlres.h>
 #include <wx/string.h>
 #include <wx/gdicmn.h>
@@ -7,58 +9,99 @@
 #include <wx/statbox.h>
 #include <wx/sizer.h>
 #include <wx/button.h>
+#include <wx/stattext.h>
+
+#include "AppPaths.h"
+#include "common/AppTheme.h"
+
+wxPanel* ScriptNameDialog::CreateSectionHeader(wxWindow* parent, const wxString& text, const wxString& lightIcon,
+                                                const wxString& darkIcon)
+{
+    const auto header = new wxPanel(parent, wxID_ANY);
+    header->SetBackgroundColour(AppTheme::ControlBackground());
+
+    const auto headerSizer = new wxBoxSizer(wxHORIZONTAL);
+    const wxSize iconSize(24, 24);
+    const wxString icon = AppTheme::IsDark() ? darkIcon : lightIcon;
+    const wxBitmapBundle iconBundle = wxBitmapBundle::FromSVGFile(AppPaths::ResourceFile({"Icons", icon}), iconSize);
+    const auto iconBitmap = new wxStaticBitmap(header, wxID_ANY, iconBundle.GetBitmap(iconSize));
+    iconBitmap->SetBackgroundColour(AppTheme::ControlBackground());
+    headerSizer->Add(iconBitmap, 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxTOP | wxBOTTOM, 12);
+
+    const auto title = new wxStaticText(header, wxID_ANY, text);
+    wxFont titleFont = title->GetFont();
+    titleFont.SetWeight(wxFONTWEIGHT_BOLD);
+    titleFont.SetPointSize(titleFont.GetPointSize() + 1);
+    title->SetFont(titleFont);
+    title->SetBackgroundColour(AppTheme::ControlBackground());
+    title->SetForegroundColour(AppTheme::Foreground());
+    headerSizer->Add(title, 1, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 10);
+
+    header->SetSizer(headerSizer);
+    header->SetMinSize(wxSize(-1, 52));
+    return header;
+}
 
 ScriptNameDialog::ScriptNameDialog(wxWindow* parent, const wxWindowID id, const wxString& title, const wxPoint& pos,
-                                   const wxSize& size, const long style) : wxDialog(parent, id, wxGetTranslation(title), pos, size, style)
+                                   const wxSize& size, const long style)
+    : wxDialog(parent, id, wxGetTranslation(title), pos, size, style)
 {
-    this->SetSizeHints(wxSize(600, 220), wxDefaultSize);
+    const wxSize minimumDialogSize(520, 300);
+    this->SetSizeHints(minimumDialogSize, wxDefaultSize);
 
     const auto mainSizer = new wxBoxSizer(wxVERTICAL);
+    const auto mainPanel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+    const auto panelSizer = new wxBoxSizer(wxVERTICAL);
+    panelSizer->Add(CreateSectionHeader(mainPanel, _("Create a new script"), "new_light.svg", "new_dark.svg"),
+                    0, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, 5);
 
-    const auto scriptNameSizer = new wxStaticBoxSizer(new wxStaticBox(this, wxID_ANY, _("Set a name for the script")), wxHORIZONTAL);
+    const auto scriptNameSizer = new wxStaticBoxSizer(new wxStaticBox(mainPanel, wxID_ANY, _("Set a name for the script")), wxHORIZONTAL);
 
-    _scriptNameText = new wxStaticText(scriptNameSizer->GetStaticBox(), wxID_ANY, _("Script name: "), wxDefaultPosition, wxDefaultSize, 0);
-    _scriptNameText->Wrap(-1);
-    scriptNameSizer->Add(_scriptNameText, 0, wxALL, 5);
+    const auto scriptNameText = new wxStaticText(scriptNameSizer->GetStaticBox(), wxID_ANY, _("Script name: "));
+    scriptNameSizer->Add(scriptNameText, 0, wxALIGN_CENTER_VERTICAL | wxALL, 5);
 
     _scriptNameCtrl = new wxTextCtrl(scriptNameSizer->GetStaticBox(), wxID_ANY, "MyNewScript", wxDefaultPosition, wxDefaultSize, 0);
     scriptNameSizer->Add(_scriptNameCtrl, 1, wxALL, 5);
+    panelSizer->Add(scriptNameSizer, 0, wxEXPAND | wxALL, 10);
 
-    mainSizer->Add(scriptNameSizer, 0, wxEXPAND, 5);
+    const auto buttonSizer = new wxBoxSizer(wxHORIZONTAL);
+    buttonSizer->AddStretchSpacer();
+    _okButton = new wxButton(mainPanel, wxID_OK);
+    buttonSizer->Add(_okButton, 0, wxALL, 5);
+    _cancelButton = new wxButton(mainPanel, wxID_CANCEL);
+    buttonSizer->Add(_cancelButton, 0, wxALL, 5);
+    panelSizer->Add(buttonSizer, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 10);
 
-    _buttonsSizer = new wxStdDialogButtonSizer();
-    _buttonsSizerOk = new wxButton(this, wxID_OK);
-    _buttonsSizer->AddButton(_buttonsSizerOk);
-    _buttonsSizerCancel = new wxButton(this, wxID_CANCEL);
-    _buttonsSizer->AddButton(_buttonsSizerCancel);
-    _buttonsSizer->Realize();
-
-    mainSizer->Add(_buttonsSizer, 0, wxEXPAND, 5);
-
-
+    mainPanel->SetSizer(panelSizer);
+    mainSizer->Add(mainPanel, 1, wxEXPAND | wxALL, 1);
     this->SetSizer(mainSizer);
+    mainSizer->Fit(this);
+    const wxSize initialSize(std::max(size.GetWidth(), minimumDialogSize.GetWidth()),
+                             std::max(size.GetHeight(), minimumDialogSize.GetHeight()));
+    this->SetSize(initialSize);
+    this->SetMinSize(initialSize);
     this->wxTopLevelWindowBase::Layout();
     this->Centre(wxBOTH);
 
     // Connect Events
-    _buttonsSizerCancel->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnCancel, this);
-    _buttonsSizerOk->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnOk, this);
+    _cancelButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnCancel, this);
+    _okButton->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnOk, this);
 }
 
 ScriptNameDialog::~ScriptNameDialog()
 {
     // Disconnect Events
-    _buttonsSizerCancel->Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnCancel, this);
-    _buttonsSizerOk->Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnOk, this);
+    _cancelButton->Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnCancel, this);
+    _okButton->Unbind(wxEVT_COMMAND_BUTTON_CLICKED, &ScriptNameDialog::OnOk, this);
 }
 
 void ScriptNameDialog::OnCancel(wxCommandEvent&)
 {
-    this->EndModal(0);
+    this->EndModal(wxID_CANCEL);
 }
 void ScriptNameDialog::OnOk(wxCommandEvent&)
 {
-    this->EndModal(1);
+    this->EndModal(wxID_OK);
 }
 wxString ScriptNameDialog::GetScriptName() const
 {
--- wxChaos/gui/wx/scripting/ScriptNameDialog.h
+++ wxChaos/gui/wx/scripting/ScriptNameDialog.h
@@ -3,7 +3,6 @@
 #include <wx/string.h>
 #include <wx/listbox.h>
 #include <wx/gdicmn.h>
-#include <wx/stattext.h>
 #include <wx/stc/stc.h>
 #include <wx/richtext/richtextctrl.h>
 #include <wx/statbmp.h>
@@ -15,17 +14,39 @@
 */
 class ScriptNameDialog : public wxDialog
 {
-    wxStaticText* _scriptNameText;
-    wxTextCtrl* _scriptNameCtrl;
-    wxStdDialogButtonSizer* _buttonsSizer;
-    wxButton* _buttonsSizerOk;
-    wxButton* _buttonsSizerCancel;
+    wxTextCtrl* _scriptNameCtrl;    ///< Input field for the new script name.
+    wxButton* _okButton;            ///< Confirms the script name.
+    wxButton* _cancelButton;        ///< Closes the dialog without creating a script.
 
+    /// @brief Creates a themed header with an icon and title.
+    /// @param parent Parent window for the header.
+    /// @param text Header title.
+    /// @param lightIcon SVG icon used in the light theme.
+    /// @param darkIcon SVG icon used in the dark theme.
+    /// @return The configured header panel.
+    [[nodiscard]] static wxPanel* CreateSectionHeader(wxWindow* parent, const wxString& text,
+                                                       const wxString& lightIcon, const wxString& darkIcon);
+    /// @brief Cancels script creation.
+    /// @param event Button click event.
     void OnCancel(wxCommandEvent& event);
+    /// @brief Confirms the entered script name.
+    /// @param event Button click event.
     void OnOk(wxCommandEvent& event);
 public:
+    /**
+     * @brief Creates the script-name dialog.
+     * @param parent Parent window.
+     * @param id Window identifier.
+     * @param title Localized dialog title.
+     * @param pos Initial dialog position.
+     * @param size Initial dialog size.
+     * @param style wxWidgets dialog style flags.
+     */
     explicit ScriptNameDialog(wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString,
                               const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize(600, 220), long style = wxDEFAULT_DIALOG_STYLE);
+    /// @brief Disconnects dialog event handlers.
     ~ScriptNameDialog() override;
+    /// @brief Gets the script name entered by the user.
+    /// @return The current script-name field value.
     [[nodiscard]] wxString GetScriptName() const;
 };
